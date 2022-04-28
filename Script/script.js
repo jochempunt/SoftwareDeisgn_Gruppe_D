02d77baec014cf88510db7d9f03dca454c2c5156
jochempunt <80535619+jochempunt@users.noreply.github.com>
2022-04-28 21:21:25 +0200

    created classes with empty functions and eslint rules

--- Script/script.js
+++ Script/script.js
@@ -1,2 +1,30 @@
 "use strict";
+class unregisteredUser {
+    ip;
+    fillOutQuestionaire(_questionaire) { }
+    searchForQuestionaire(_questionaireName) { }
+    viewStatistics() { }
+    signUp(_username, _password) { }
+}
+class registeredUser extends unregisteredUser {
+    username;
+    password;
+    getQuestionaires() { }
+    signIn(_username, _password) { }
+    createQuestionaire() { }
+}
+class Questionaire {
+    title;
+    timespan;
+    questions;
+    isValid;
+    showQuestionaire() { }
+    addQuestion(_question) { }
+    showStatistics(_valid) { }
+    finalisedQuestionaire() { }
+}
+class Question {
+    text;
+    getAnswers() { }
+}
 //# sourceMappingURL=script.js.map
--- Script/script.ts
+++ Script/script.ts
@@ -1,19 +1,60 @@
-namespace klassen{
+class unregisteredUser{
+    ip: string;
 
-
-
-
-
-
-
-
+    fillOutQuestionaire(_questionaire: Questionaire): void{}
     
+    searchForQuestionaire(_questionaireName: string): void{}
 
+    viewStatistics(): void{}
 
-
-
-
-
-
-    
+    signUp(_username: string, _password: string): void{}
 }
+
+class registeredUser extends unregisteredUser{
+    username: string;
+    password: string;
+
+
+    getQuestionaires(): Questionaire[]{}
+
+    signIn(_username: string, _password:string): void{}
+
+    createQuestionaire(): void{}
+
+}
+
+
+
+class Questionaire{
+    title: string;
+    timespan: TimeRanges;
+    questions: Question[];
+    isValid: boolean;
+
+    showQuestionaire(): void{}
+
+    addQuestion(_question: Question): void{}
+
+    showStatistics(_valid: boolean): void{}
+
+    finalisedQuestionaire(): void {}
+
+
+}
+
+class Question{
+    text: string;
+
+    getAnswers(): string[]{}
+
+}
+
+    
+
+
+
+
+
+
+
+
